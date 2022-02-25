c23e767fed11ac819ef301df17a1f4da1253fb1c
Arnaud Stien <arnaud.stien@leroymerlin.fr>
2022-02-25 16:54:15 +0100

    mongoDb

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,45 @@ const client = new MongoClient(databaseUrl);
 client.connect().then(async (client) => {
   const db = client.db();
 
-  await db.createCollection("newData");
+  await db.createCollection("Movies");
+  db.createCollection("Movies", {
+    validator: {
+      $jsonSchema: {
+        bsonType: "object",
+        required: ["Title", "type", "synopsis"],
+        properties: {
+          _id: { bsonType: "objectId" },
+          Title: {
+            bsonType: "string",
+            description: "must be a string and is required",
+          },
+          Type: {
+            bsonType: "string",
+            description: "must be a string and is required",
+          },
+          Synopsis: {
+            bsonType: "string",
+            description: "must be an integer > 18 and is required",
+          },
+        },
+      },
+    },
+  });
+
+  //const newUsers = [
+  //   {
+  //     Title: "Francis",
+  //     Type: "Ewlines",
+  //     synopsis: "francis@fewlines.co",
+  //   },
+
+  //   {
+  //     Title: "Farida",
+  //     Type: "Ewlines",
+  //     Synopsis: "farida@fewlines.co",
+  //   },
+  // ];
+  // db.collections("Movies").insertMany(newUsers);
+
   client.close();
 });
